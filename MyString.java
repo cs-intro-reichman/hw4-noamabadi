a4e2bc2ad1642cd1b099d15327bba845c0fc3c77
noamabadi <noam.abadi@post.runi.ac.il>
2024-12-05 16:29:23 +0200

    completed

--- MyString.java
+++ MyString.java
@@ -18,13 +18,37 @@ public class MyString {
 
     /** Returns the lowercase version of the given string. */
     public static String lowerCase(String str) {
-        // Replace the following statement with your code
-        return null;
+        String result = "";
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch >= 'A' && ch <= 'Z') {
+                ch = (char) (ch + ('a' - 'A'));
+                
+            }
+            result += ch;
+        }
+        return result;
     }
 
     /** If str1 contains str2, returns true; otherwise returns false. */
     public static boolean contains(String str1, String str2) {
-        // Replace the following statement with your code
+        if (str2.length() == 0) {
+            return true;    
+        }
+
+        for (int i = 0; i <= str1.length() - str2.length(); i++) {
+            boolean match = true;
+            for (int j = 0; j < str2.length(); j++) {
+                if (str1.charAt(i + j) != str2.charAt(j)) {
+                    match = false;
+                    break;        
+               }
+            }
+            if (match) {
+                return true;        
+            }
+                }
+          
         return false;
     }
 }
